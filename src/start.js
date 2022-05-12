4daa5fc05df9abc97fd343b72c08b1333157fd10
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 01:16:32 -0300

    Prefix start

--- src/start.js
+++ src/start.js
@@ -1,7 +1,8 @@
 const {bot} = require("./client.js")
+const { prefix } = require("../config.json")
 
 bot.on("ready", ()=>{
     console.log( "\033[34m" + "O bot foi iniciado com sucesso!" + "\033[0;0m")
     console.log("Está em: " + "\033[1;34m" +bot.guilds.cache.size + " servidores." + "\033[0m")
-    bot.user.setActivity("!ajuda: Para ajuda!")
+    bot.user.setActivity(`${prefix}ajuda: Para ajuda!`)
 })
